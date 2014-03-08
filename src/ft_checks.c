10eff19dff7c3ad361b4a30eddac84c335de9af4
Balthazar GRONON <bgronon@gmail.com>
2014-03-09 00:16:26 +0100

    presentation

--- src/ft_checks.c
+++ src/ft_checks.c
@@ -6,7 +6,7 @@
 /*   By: bgronon <bgronon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 20:20:49 by bgronon           #+#    #+#             */
-/*   Updated: 2014/03/09 00:12:25 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/09 00:14:02 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ int			ft_check_col(int col)
 
 static int	ft_look_win(t_env *env, int i, int j, char c)
 {
-	if (ft_look_up(env, i, j, c) == 4 || ft_look_down(env, i, j, c) == 4
-		|| ft_look_left(env, i, j, c) == 4 || ft_look_right(env, i, j, c) == 4
+	if (ft_look_up(env, i, j, c) == 4
+		|| ft_look_down(env, i, j, c) == 4
+		|| ft_look_left(env, i, j, c) == 4
+		|| ft_look_right(env, i, j, c) == 4
 		|| ft_look_up_right(env, i, j, c) == 4
 		|| ft_look_up_left(env, i, j, c) == 4
 		|| ft_look_down_right(env, i, j, c) == 4
