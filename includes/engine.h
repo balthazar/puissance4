63617c33c594ac6d549b4297eb0cfdac51824e44
Pierre-Marie COTASSON <pcotasso@e3r4p7.42.fr>
2014-03-08 14:51:41 +0100

    maj

--- includes/engine.h
+++ includes/engine.h
@@ -6,7 +6,7 @@
 /*   By: pcotasso <pcotasso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 14:21:59 by pcotasso          #+#    #+#             */
-/*   Updated: 2014/03/08 14:23:35 by pcotasso         ###   ########.fr       */
+/*   Updated: 2014/03/08 14:51:24 by pcotasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define ENGINE_H
 
 # include "puissance.h"
+
+# define	UP			1
+# define	UPRIGHT		2
+# define	RIGHT		3
+# define 	RIGHTDOWN	4
+
+# define	HU_WIN		1337
+# define	AI_WIN		4008
 
 int		check_win(t_env *env)
 
