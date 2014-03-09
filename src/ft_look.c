0744cb9c7f0700aa4a821c43c0a47380e5e3143e
Balthazar GRONON <bgronon@gmail.com>
2014-03-09 14:15:56 +0100

    fix looks

--- src/ft_look.c
+++ src/ft_look.c
@@ -6,7 +6,7 @@
 /*   By: bgronon <bgronon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 22:23:19 by bgronon           #+#    #+#             */
-/*   Updated: 2014/03/09 13:45:10 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/09 14:15:09 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,72 +16,84 @@ int			ft_look_left(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		cpt = ft_look_right(i, j, c, 0);
+		revert = ft_look_right(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (j - cpt) >= 0)
 	{
 		if (env->board[i][j - cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_right(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		cpt = ft_look_left(i, j, c, 0);
+		revert = ft_look_left(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (j + cpt) < env->column)
 	{
 		if (env->board[i][j + cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_up(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		cpt = ft_look_down(i, j, c, 0);
+		revert = ft_look_down(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i - cpt) >= 0)
 	{
 		if (env->board[i - cpt][j] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_down(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		cpt = ft_look_up(i, j, c, 0);
+		revert = ft_look_up(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i + cpt) < env->row)
 	{
 		if (env->board[i + cpt][j] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_around(t_env *env, int i, int j)
--- src/ft_look_diagonals.c
+++ src/ft_look_diagonals.c
@@ -6,7 +6,7 @@
 /*   By: bgronon <bgronon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/09 11:20:37 by bgronon           #+#    #+#             */
-/*   Updated: 2014/03/09 13:47:17 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/09 14:14:30 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,70 +16,82 @@ int			ft_look_up_right(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		ft_look_down_left(i, j, c, 0);
+		revert = ft_look_down_left(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i - cpt) >= 0 && (j + cpt) < env->column)
 	{
 		if (env->board[i - cpt][j + cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_up_left(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		ft_look_down_right(i, j, c, 0);
+		revert = ft_look_down_right(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i - cpt) >= 0 && (j - cpt) >= 0)
 	{
 		if (env->board[i - cpt][j - cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_down_right(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		ft_look_up_left(i, j, c, 0);
+		revert = ft_look_up_left(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i + cpt) < env->row && (j + cpt) < env->column)
 	{
 		if (env->board[i + cpt][j + cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
 
 int			ft_look_down_left(int i, int j, char c, int repeat)
 {
 	t_env	*env;
 	int		cpt;
+	int		revert;
 
 	env = ft_get_env();
 	cpt = 0;
+	revert = 0;
 	if (repeat)
-		ft_look_up_right(i, j, c, 0);
+		revert = ft_look_up_right(i, j, c, 0) - 1;
+	revert = (revert < 0) ? 0 : revert;
 	while (cpt <= 3 && (i + cpt) < env->row && (j - cpt) >= 0)
 	{
 		if (env->board[i + cpt][j - cpt] != c)
-			return (cpt);
+			return (cpt + revert);
 		++cpt;
 	}
-	return (cpt);
+	return (cpt + revert);
 }
