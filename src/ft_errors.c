c4c5ab4d281b0bc53af076529e2d72503c178ea0
Balthazar GRONON <bgronon@gmail.com>
2014-03-09 18:47:45 +0100

    max size

--- src/ft_errors.c
+++ src/ft_errors.c
@@ -6,7 +6,7 @@
 /*   By: bgronon <bgronon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 19:40:01 by bgronon           #+#    #+#             */
-/*   Updated: 2014/03/09 13:02:20 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/09 18:43:39 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_error(int type)
 	if (type == ER_MALLOC)
 		ft_putendl_fd("Malloc error\n", 2);
 	else if (type == ER_SIZE)
-		ft_putstr_fd("Not enough rows or columns\n", 2);
+		ft_putstr_fd("Problem with rows or columns\n", 2);
 	else if (type == ER_USAGE)
 		ft_putstr_fd("usage: puissance-4 [rowsize] [columnsize] ...\n", 2);
 }
